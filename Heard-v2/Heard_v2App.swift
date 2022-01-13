feb278e3a9cef464ce767c012877a583032e8b2b
zacwal13 <zacharywalsh13@gmail.com>
2022-01-12 20:59:47 -0600

    Initial Commit

---
+++ Heard-v2/Heard_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Heard_v2App.swift
+//  Heard-v2
+//
+//  Created by Zachary Walsh on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Heard_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
